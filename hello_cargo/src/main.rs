ccf56dde6ffc675a6b57fe2a3579c12a8d1046ba
LaughingLemon <laughing.programmer@outlook.com>
2021-04-26 18:13:40 +0100

    File open error

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,10 +1,12 @@
+use std::fs::File;
+use std::io::Error;
+
 fn main() {
-    //panic!("let's panic");
-
-    let v = vec![1, 2, 3];
-
-    v[99];
-
-    let h1 = "something";
-    println!("{:?}", h1);
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem opening the file: {:?}", error)
+        },
+    };
 }
