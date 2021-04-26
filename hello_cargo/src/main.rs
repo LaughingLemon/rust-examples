d79d5e61c07cde6cef9c44f2d9fa90179a8b0949
LaughingLemon <laughing.programmer@outlook.com>
2021-04-26 20:47:45 +0100

    ... and character

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,18 +2,30 @@
 fn main() {
     let v = vec!(1, 3, 7, 3, 12, 54, 34, 67, 2);
 
-    let largest = find_largest(&v);
+    let largest = largest_integer(&v);
 
     println!("largest is {}", largest);
 
-    let v = vec!(12, 65, 45, 87, 3);
+    let v = vec!('e','G', 'b', 'L');
 
-    let largest = find_largest(&v);
+    let largest = largest_character(&v);
 
     println!("largest is {}", largest);
 }
 
-fn find_largest(v: &[i32]) -> i32 {
+fn largest_integer(v: &[i32]) -> i32 {
+    let mut largest = v[0];
+
+    for &i in v.iter() {
+        if i > largest {
+            largest = i;
+        }
+    }
+    largest
+}
+
+
+fn largest_character(v: &[char]) -> char {
     let mut largest = v[0];
 
     for &i in v.iter() {
