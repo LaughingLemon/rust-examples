474bae87fdc5a9ea1eeaa47f3605338072432654
LaughingLemon <laughing.programmer@outlook.com>
2021-04-26 19:04:50 +0100

    Handle file errors

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,12 +1,16 @@
 use std::fs::File;
-use std::io::Error;
+use std::io::{Error, ErrorKind};
 
 fn main() {
     let f = File::open("hello.txt");
     let f = match f {
         Ok(file) => file,
-        Err(error) => {
-            panic!("Problem opening the file: {:?}", error)
-        },
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(er) => panic!("Problem creating the file: {:?}", er)
+            },
+            other_error => panic!("Problem opeining the file: {:?}", other_error)
+        }
     };
 }
