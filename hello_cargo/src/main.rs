04ef9343db9f6effdd870b8627a0304d6f4ef6a0
LaughingLemon <laughing.programmer@outlook.com>
2021-04-26 17:59:25 +0100

    Basic panics

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,31 +1,10 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut h = HashMap::new();
+    //panic!("let's panic");
 
-    h.insert(String::from("key 1"), 1);
-    h.insert(String::from("key 2"), 2);
+    let v = vec![1, 2, 3];
 
-    let k1 = String::from("key 3");
-    let v1 = 3;
+    v[99];
 
-    let mut h1 = HashMap::new();
-
-    h1.insert(k1, v1);
-
-    println!("{:?}", h1);
-
-    h1.insert("key 3".to_string(), 12);
-
-    println!("{:?}", h1);
-
-    h1.entry("key 3".to_string()).or_insert(13);
-    h1.entry("key 4".to_string()).or_insert(10);
-
-    println!("{:?}", h1);
-
-    let v2 = h1.entry("key 4".to_string()).or_insert(10);
-    *v2 += 5;
-
+    let h1 = "something";
     println!("{:?}", h1);
 }
