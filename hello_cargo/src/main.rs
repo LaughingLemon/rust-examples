7810161421a4d0e7ac97a05393d41c4aa1b41fc0
LaughingLemon <laughing.programmer@outlook.com>
2021-04-26 15:21:59 +0100

    Using format!

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let s1 = String::from("hello ");
 
-    let s2= "world";
-    let s3 = s1 + s2;
+    let s2= String::from("world");
+    let s3 = s1 + &s2;
 
     println!("This is {}, {}", s2, s3);
+
+    let s4 = format!("{}, {}", s2, s3);
+    println!("{}", s4);
 }
