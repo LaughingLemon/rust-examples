ab09ae1949cb975bd9b51df5b01b10f04aa5eb5d
LaughingLemon <laughing.programmer@outlook.com>
2021-05-01 10:48:59 +0100

    Closure Cache

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,18 +1,39 @@
 
 fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
+    let mut expensive_closure = Cacher::new(
+        |num| {
+            println!("being called with {}", num);
+            num
+        }
+    );
+
+    println!("Expensive closure is {}", expensive_closure.value(5));
+    println!("Expensive closure is {}", expensive_closure.value(5));
+    println!("Expensive closure is {}", expensive_closure.value(7));
+    println!("Expensive closure is {}", expensive_closure.value(5));
 }
 
-fn longest<'a>(first: &'a str, next: &'a str) -> &'a str {
-    if first.len() > next.len() {
-        first
-    } else {
-        next
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
     }
 }
