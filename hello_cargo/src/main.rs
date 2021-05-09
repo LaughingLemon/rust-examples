2cea14147f54ee888357a3eeda0e412c1af4d028
LaughingLemon <laughing.programmer@outlook.com>
2021-05-09 14:56:56 +0100

    Using lambda

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,39 +1,9 @@
 
 fn main() {
-    let mut expensive_closure = Cacher::new(
-        |num| {
-            println!("being called with {}", num);
-            num
-        }
-    );
+    let v1 = vec!(1, 2, 3);
 
-    println!("Expensive closure is {}", expensive_closure.value(5));
-    println!("Expensive closure is {}", expensive_closure.value(5));
-    println!("Expensive closure is {}", expensive_closure.value(7));
-    println!("Expensive closure is {}", expensive_closure.value(5));
+    let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+
+    println!("List is {:?}", v2);
 }
 
-struct Cacher<T> where T: Fn(u32) -> u32 {
-    calculation: T,
-    value: Option<u32>
-}
-
-impl<T> Cacher<T> where T: Fn(u32) -> u32 {
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
