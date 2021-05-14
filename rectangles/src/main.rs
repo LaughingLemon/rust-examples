e93465bf95c9afbd160da69fff2d4629d1a357e6
LaughingLemon <laughing.programmer@outlook.com>
2021-05-14 19:43:00 +0100

    Add unit tests

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,16 +9,37 @@ fn main() {
 
 #[derive(Debug)]
 struct Rectangle {
-    width: u32,
-    height: u32
+    pub width: u32,
+    pub height: u32
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
+    pub fn area(&self) -> u32 {
         self.width * self.height
     }
 
-    fn can_hold(&self, other: &Rectangle) -> bool {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_hold_rectangle() {
+        let rect1 = Rectangle{ width: 50, height: 200 };
+        let rect2 = Rectangle{ width: 15, height: 100 };
+
+        assert!(rect1.can_hold(&rect2));
+    }
+
+    #[test]
+    fn cant_hold_rectangle() {
+        let rect1 = Rectangle{ width: 50, height: 200 };
+        let rect3 = Rectangle{ width: 200, height: 150 };
+
+        assert!(!rect1.can_hold(&rect3));
     }
 }
