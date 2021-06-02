b5c7469a1d45b88f4213fd69f59135889d2361fb
LaughingLemon <laughing.programmer@outlook.com>
2021-06-02 20:35:58 +0100

    Recieving a message stream

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,17 +1,28 @@
 use std::thread;
 use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     let handle = thread::spawn(move || {
-        let val = String::from("something");
-        println!("send something {}", val);
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("something"),
+            String::from("something"),
+            String::from("oranges"),
+            String::from("something"),
+        ];
+        println!("send something {:?}", vals);
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(2));
+        }
     });
 
-    let val = rx.recv().unwrap();
-    println!("get something {}", val);
+    for rec in rx {
+        println!("get something {}", rec);
+    }
 
     handle.join().unwrap();
 }
