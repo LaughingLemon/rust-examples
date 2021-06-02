a77004cfadd479bfd972e61ef062c0ebdedb8c96
LaughingLemon <laughing.programmer@outlook.com>
2021-06-02 11:13:29 +0100

    Thread sharing vector with main thread

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,17 +1,11 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1 .. 20 {
-            println!("Hi! Number {} from spawned thread", i);
-            thread::sleep(Duration::from_millis(500));
-        }
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("here is the vector {:?}", v);
     });
-    for i in 1 .. 5 {
-        println!("Hi! Number {} from main thread", i);
-        thread::sleep(Duration::from_secs(2));
-    }
 
     handle.join().unwrap();
 }
