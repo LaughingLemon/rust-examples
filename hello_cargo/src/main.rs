131816785e13ae77e2bf78889f01c5c4f2d93fa9
LaughingLemon <laughing.programmer@outlook.com>
2021-06-02 20:46:18 +0100

    Multiple producers

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+    let tx2 = mpsc::Sender::clone(&tx);
 
     let handle = thread::spawn(move || {
         let vals = vec![
@@ -20,10 +21,26 @@ fn main() {
         }
     });
 
+    let handle2 = thread::spawn(move || {
+        let vals = vec![
+            String::from("send"),
+            String::from("three"),
+            String::from("and"),
+            String::from("fourpence"),
+        ];
+        println!("send something {:?}", vals);
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(2));
+        }
+    });
+
     for rec in rx {
         println!("get something {}", rec);
     }
 
     handle.join().unwrap();
+    handle2.join().unwrap();
 }
 
