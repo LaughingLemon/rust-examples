6c0e7bf9be28726f35e32e2931f555663138e086
LaughingLemon <laughing.programmer@outlook.com>
2021-06-09 16:23:36 +0100

    Defining and using traits to demonstrate OOP

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,23 +1,27 @@
-use std::thread;
-use std::sync::{Mutex, Arc};
+use hello_cargo::{Draw, Screen};
+
+pub struct Button {
+    pub width: i32,
+    pub height: i32,
+    pub label_text: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("draw Button, w: {}, h: {}, labelText: {}",
+                 self.width, self.height, self.label_text);
+    }
+}
 
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
+    let screen = Screen {
+        components: vec![Box::new(Button{
+            width: 10,
+            height: 50,
+            label_text: "Something".to_string()
+        })]
+    };
 
-    for _ in 1 .. 10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
-            *num += 1;
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("{}", *counter.lock().unwrap());
+    screen.run();
 }
 
