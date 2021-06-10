57f154e026b2adac8838ced4fe51f1831ebbd3cb
LaughingLemon <laughing.programmer@outlook.com>
2021-06-10 18:41:15 +0100

    Enum pattern matching

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,39 +1,18 @@
 
-struct Point {
-    x: i32,
-    y: i32
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColour(i32, i32, i32)
 }
 
 fn main() {
-    let x = Some(6);
-    let y = 10;
-
-    match x {
-        Some(50) => println!("Some(50)"),
-        Some(y) => println!("This is y {}", y),
-        _ => println!("This is the default {:?}", x)
-    }
-    println!("Some(x): {:?}, y: {}", x, y);
-
-    let x = 1;
-    match x {
-        1 | 2 => println!("One or two"),
-        3 => println!("three"),
-        _ => println!("something else")
-    }
-
-    let x = 3;
-    match x {
-        1..=5 => println!("One to five"),
-        _ => println!("something else")
-    }
-
-    let p = Point { x: 0, y: 12 };
-
-    match p {
-        Point { x, y: 0 } => println!("On the y axis, x: {}", x),
-        Point {x: 0, y} => println!("On the x axis, y: {}", y),
-        Point { x, y} => println!("x:{}, y: {}", x, y)
+    let msg = Message::ChangeColour(1, 2, 3);
+    match msg {
+        Message::Quit => (),
+        Message::ChangeColour(r, g, b) => println!("msg: ChangeColour {}, {}, {}", r, g, b ),
+        Message::Move {x, y} => println!("msg: Move {}, {}", x, y),
+        Message::Write(txt) => println!("msg: Write {}", txt)
     }
 }
 
