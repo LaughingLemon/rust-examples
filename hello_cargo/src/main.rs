86f88bae6fa0248143d6d0581c1c0d5cc8931c0b
LaughingLemon <laughing.programmer@outlook.com>
2021-06-10 19:57:51 +0100

    Nested Enum pattern matching

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,18 +1,23 @@
+enum Colour {
+    Rgb(i32, i32, i32),
+    Hsl(i32, i32, i32),
+}
 
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColour(i32, i32, i32)
+    ChangeColour(Colour)
 }
 
 fn main() {
-    let msg = Message::ChangeColour(1, 2, 3);
+    let msg = Message::ChangeColour(Colour::Rgb(1, 2, 4));
     match msg {
-        Message::Quit => (),
-        Message::ChangeColour(r, g, b) => println!("msg: ChangeColour {}, {}, {}", r, g, b ),
-        Message::Move {x, y} => println!("msg: Move {}, {}", x, y),
-        Message::Write(txt) => println!("msg: Write {}", txt)
+        Message::ChangeColour(Colour::Rgb(r, g, b)) =>
+            println!("msg: ChangeColour, RGB {}, {}, {}", r, g, b ),
+        Message::ChangeColour(Colour::Hsl(h, s, l)) =>
+            println!("msg: ChangeColour, HSL {}, {}, {}", h, s, l ),
+        _ => ()
     }
 }
 
