5294b57e01fff5c581a515344b7013b00fd86b50
LaughingLemon <laughing.programmer@outlook.com>
2021-06-18 12:21:25 +0100

    Unsafe references

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,13 +1,14 @@
 
 fn main() {
-    let x = Some(6);
-    let y = 10;
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
 
-    match x {
-        Some(50) => println!("Some(50)"),
-        Some(n) if n == y => println!("This is y {}", y),
-        _ => println!("This is the default {:?}", x)
+    unsafe {
+        //unsafe allows us to set the mutable references above
+        *r2 = 12;
+
+        println!("r1: {}, r2: {}", *r1, *r2);
     }
-    println!("Some(x): {:?}, y: {}", x, y);
 }
 
