47401126bd62baea1279f99d16607d7fd1844c0d
LaughingLemon <laughing.programmer@outlook.com>
2021-06-23 18:12:44 +0100

    Implementing Display trait

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,14 +1,38 @@
+use std::fmt;
+use std::fmt::Formatter;
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+struct Point {
+    x: u32,
+    y: u32
+}
+
+impl OutlinePrint for Point {}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {}, {} )", self.x, self.y)
+    }
+}
 
 fn main() {
-    let mut num = 5;
-    let r1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
+    let pnt = Point{
+        x: 3,
+        y: 5
+    };
 
-    unsafe {
-        //unsafe allows us to set the mutable references above
-        *r2 = 12;
-
-        println!("r1: {}, r2: {}", *r1, *r2);
-    }
+    pnt.outline_print();
 }
 
